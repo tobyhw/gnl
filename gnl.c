2e4c327e4191c75c6400734439015327705fdb28
Toby Haass Walpert <thwalpert@simusolar.com>
2018-04-25 10:49:03 +0200

    gnl functional

---
+++ gnl.c
@@ -0,0 +1,58 @@
+#include "gnl.h"
+
+int     readsave(t_glst *c, char **out, t_gnl i, int r)
+{
+    if (i.l >= 0 && c->sv[i.l] == '\n' && (*out = (char*)malloc(i.l + 1)))
+    {
+        c->sv = (c->s > i.l ? (char*)malloc(c->s - i.l) : NULL);
+        if ((c->s = -1) && (c->sv || ++c->s))
+            while (i.tmp[++c->s + i.l + 1] || (c->sv[c->s] = 0))
+                c->sv[c->s] = i.tmp[c->s + i.l + 1];
+        while (i.l >= 0 && ((i.tmp[i.l] == '\n' && ((*out)[i.l] = 0)) || i.l--))
+            (*out)[i.l] = i.tmp[i.l];
+        free(i.tmp);
+        return (-1);
+    }
+    if ((i.buf = (char*)malloc(BUFF_SIZE)))
+        if ((r = read(c->fd, i.buf, BUFF_SIZE)) && r > 0)
+            if ((c->sv = (char*)malloc(r + c->s + 1)))
+                i.l = r + c->s;
+    if (c->sv && r >= 0 && (!(c->sv[i.l] = (r == 0 ? '\n' : 0)) || c->s > 0))
+        while (r-- && (c->sv[r + c->s] = i.buf[r]))
+            while (!r && (c->s-- || !(c->s = i.l) || 0))
+                if ((c->sv[c->s] = i.tmp[c->s]) && !c->s)
+                    free(i.tmp);
+    if (!((!i.buf || (r && !c->sv)) && !(r = 1)))
+        if (!c->fd && c->s && c->s % BUFF_SIZE && c->sv[c->s - 1] != '\n')
+            c->sv[c->s] = '\n';
+    free(i.buf);
+    return (r);
+}
+
+int     get_next_line(const int fd, char **out)
+{
+    static t_glst   *f = NULL;
+    t_glst          *c;
+    t_gnl           i;
+
+    c = f;
+    i.l = -1;
+    while ((!c && out) || (c && c->fd != fd && ((c = c->next) || !c)))
+        if (!c && (c = (!out ? NULL : (t_glst*)malloc(sizeof(t_glst)))))
+        {
+            c->fd = (int)fd;
+            c->next = f;
+            c->sv = NULL;
+            c->s = 0;
+            f = c;
+        }
+    while (fd >= 0 && c && i.l == -1 && ((i.tmp = c->sv) || !c->sv))
+    {
+        while (!(*out = NULL) && c->sv && ++i.l < c->s && c->sv[i.l] != '\n')
+            ;
+        i.l = readsave(c, out, i, 0);
+        if ((i.l == -1 && *out) || (!i.l && !(c->sv = NULL)))
+            return (-i.l);
+    }
+    return (-1);
+}
---
+++ main.c
@@ -0,0 +1,26 @@
+#include "gnl.h"
+
+int	main(int argc, char *argv[])
+{
+	char	*o;
+	int	fd;
+	int	r;
+
+	if (argc == 1)
+		while((r = get_next_line(0, &o)) > 0)
+			printf("%s\n",o);
+	else
+	{
+		if (argc == 2)
+			fd = open(argv[1],O_RDONLY);
+		if (argc > 2 && argv[1][0] == '-' && argv[1][1] == 'f')
+			fd = atoi(argv[2]);
+		while((r = get_next_line(fd, &o)) > 0)
+                        printf("%s\n",o);
+	}
+	if (!r && 0)
+		printf("--EOF--\n");
+	if (r)
+		printf("Error\n");
+	return (0);
+}
